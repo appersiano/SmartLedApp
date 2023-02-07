1f79c4fa79ea0c83c889cd783da9b95c3c0c428e
Alessandro Persiano <alessandro.persiano@filotrack.com>
2023-02-07 18:52:33 +0100

    Add statusBar black color

--- app/src/main/java/com/appersiano/smartledapp/ui/theme/Theme.kt
+++ app/src/main/java/com/appersiano/smartledapp/ui/theme/Theme.kt
@@ -5,9 +5,13 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalView
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
+    primary = Black,
     primaryVariant = Purple700,
     secondary = Teal200
 )
@@ -29,11 +33,24 @@ private val LightColorPalette = lightColors(
 
 @Composable
 fun SmartLedAppTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+    val systemUiController = rememberSystemUiController()
+
     val colors = if (darkTheme) {
         DarkColorPalette
     } else {
         LightColorPalette
     }
+
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            systemUiController.setStatusBarColor(
+                color = Color.Black,
+                darkIcons = false
+            )
+        }
+    }
+
 
     MaterialTheme(
         colors = colors,
